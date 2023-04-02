8a95ff1539ad70a3c64f25bfddb384d86367a143
GMartin03 <martingomez2003174@hotmail.com>
2023-04-01 21:53:31 -0300

    creado prototipo del diseño

--- src/components/room/Room.js
+++ src/components/room/Room.js
@@ -2,8 +2,11 @@ import { useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import io from 'socket.io-client';
 const socketIo = io('http://localhost:4000');
+import style from './room.module.css';
+import scrumpe from '../assets/scrumpe.png';
 
 const Room = () => {
+  const { bodyContainer, buttonArea, cardArea, userArea } = style;
   const [myCard, setMyCard] = useState();
   const [roomData, setRoomData] = useState([]);
   const cards = [0, 1, 2, 3, 5, 8, 12, 20];
@@ -13,18 +16,26 @@ const Room = () => {
 
   useEffect(() => {
     socket.current = socketIo;
-    socket.current.on('point response', (data) => updateRoomData(data.sender, data.card));
+    socket.current.on('point response', (data) =>
+      updateRoomData(data.sender, data.card)
+    );
     socket.current.on('getOthersDataBe 4', (data) => {
       updateRoomData(data.sender, data.card);
     });
     socket.current.on('getOthersDataBe 2', (data) => {
       if (data.requester !== socket.current.id) {
-        socket.current.emit('getOthersDataFe 3', {card: myCard, requester: data.requester});
+        socket.current.emit('getOthersDataFe 3', {
+          card: myCard,
+          requester: data.requester,
+        });
       }
     });
     socket.current.on('connect room response', (data) => {
       if (data.connected) {
-        socket.current.emit('getOthersDataFe 1', {room: token, socketId: socket.current.id});
+        socket.current.emit('getOthersDataFe 1', {
+          room: token,
+          socketId: socket.current.id,
+        });
       }
     });
     return () => {
@@ -45,7 +56,11 @@ const Room = () => {
   const sendCard = (point) => {
     setMyCard(point);
     updateRoomData(socket.current.id, point);
-    socket.current.emit('point', { card: point, room: token, sender: socket.current.id });
+    socket.current.emit('point', {
+      card: point,
+      room: token,
+      sender: socket.current.id,
+    });
   };
 
   const updateRoomData = (hand, card) => {
@@ -62,13 +77,27 @@ const Room = () => {
 
   return (
     <>
-      <h3>Enviar puntaje</h3>
-      {cards &&
-        cards?.map((card) => (
-          <button onClick={() => sendCard(card)} key={card}>
-            {card}
-          </button>
-        ))}
+      <div className={bodyContainer}>
+        <div className={userArea}>
+          <img src={scrumpe} />
+          <p>User 1</p>
+        </div>
+        <div className={cardArea}>
+          <button>Revelar cartas!</button>
+        </div>
+        <div className={userArea}>
+          <img src={scrumpe} />
+          <p>User 2</p>
+        </div>
+        <div className={buttonArea}>
+          {cards &&
+            cards?.map((card) => (
+              <button onClick={() => sendCard(card)} key={card}>
+                {card}
+              </button>
+            ))}
+        </div>
+      </div>
     </>
   );
 };
