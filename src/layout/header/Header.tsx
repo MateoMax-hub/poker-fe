e1c9a67fc31468f2c90b376a2ec373191783566f
GMartin03 <martingomez2003174@hotmail.com>
2023-05-16 19:44:39 -0300

    design for select and switch

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -4,7 +4,7 @@ import { GlobalContext } from '../../context/Context';
 import { Switch } from 'antd';
 
 const Header = () => {
-  const { headerContainer, selectedCard, switchBtn } = style;
+  const { headerContainer, selectedCard, switchBtn, select } = style;
   console.log(useContext(GlobalContext));
 
   return (
@@ -13,7 +13,14 @@ const Header = () => {
         <div className={selectedCard}>☕</div>
         <h1>POKER PLANNING</h1>
       </div>
-      <Switch className={switchBtn} />
+      <div>
+        <select className={select}>
+          <option>Español</option>
+          <option>English</option>
+          <option>Deutsch</option>
+        </select>
+        <Switch className={switchBtn} />
+      </div>
     </div>
   );
 };
