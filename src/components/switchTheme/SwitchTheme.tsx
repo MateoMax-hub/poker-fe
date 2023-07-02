471f6df267dc06f543276190f4e53b5392f54b08
Mateo Max <max.mateo.02@gmail.com>
2023-07-02 18:12:02 -0300

    coffee centered on cards carrousel and selected theme now is being stored on localstorage so the selected theme will be effective even if user reload the page

--- src/components/switchTheme/SwitchTheme.tsx
+++ src/components/switchTheme/SwitchTheme.tsx
@@ -1,9 +1,10 @@
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import style from './switchTheme.module.scss';
 
 const SwitchTheme = () => {
   const lightThemeText = useRef<HTMLDivElement>(null)
   const darkThemeText = useRef<HTMLDivElement>(null)
+  const switchTheme = useRef<HTMLInputElement>(null)
   const {
     themeSwitchWrapper,
     mainWrapper,
@@ -19,24 +20,36 @@ const SwitchTheme = () => {
     star6,
     disabled,
   } = style;
+  const [isDarkThemeOn, setIsDarkThemeOn] = useState(false)
+
+  useEffect(() => {
+    const theme: boolean = JSON.parse(localStorage.getItem('isThemeDarkOn') || 'false')
+    setIsDarkThemeOn(theme)
+    if (switchTheme?.current) switchTheme.current.checked = theme
+  }, [])
+
+  useEffect(() => {
+    if (isDarkThemeOn) {
+      document.documentElement.setAttribute("data-theme", "dark");
+    } else {
+      document.documentElement.setAttribute("data-theme", "light");
+    }
+  }, [isDarkThemeOn])
 
   const handleSwitch = (e: Event | React.MouseEvent<HTMLInputElement, MouseEvent>) => {
     lightThemeText.current?.classList.toggle(disabled)
     darkThemeText.current?.classList.toggle(disabled)
 
     const target = e.target as HTMLInputElement
-    if (target.checked) {
-      document.documentElement.setAttribute("data-theme", "dark");
-    } else {
-      document.documentElement.setAttribute("data-theme", "light");
-    }
+    localStorage.setItem('isThemeDarkOn', JSON.stringify(target.checked))
+    setIsDarkThemeOn(target.checked)
   }
 
   return (
     <div className={mainWrapper}>
       <div className={themeSwitchWrapper}>
         <label htmlFor="theme-btn">
-          <input type="checkbox" id="theme-btn" onClick={handleSwitch}/>
+          <input ref={switchTheme} type="checkbox" id="theme-btn" onClick={handleSwitch}/>
           <div className={sliderWrapper}>
             <div className={themeBtnSlider}></div>
             <span className={`${star} ${star1}`}></span>
